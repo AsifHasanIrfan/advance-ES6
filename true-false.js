43d3fd698d5860753fbcf3e30f9ef57c5d11dfcc
AsifHasanIrfan <asifhasanirfan@gmail.com>
2021-02-01 23:09:08 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// this is false item list //
+// 1. number = 0;
+// 2. string = "";
+// 3. undefined
+// 4. null
+// 5. NaN
+// 6. false
+
+const name = '';
+
+if(name) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
